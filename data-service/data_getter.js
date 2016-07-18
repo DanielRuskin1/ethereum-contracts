b8b46a44858d93e9c90833e1f4ec40495b6ee57c
Daniel Ruskin <daniel@druskin.co>
2016-07-17 17:24:31 -0700

    Cleanup DataRequest processor + fix a few bugs

--- data-service/data_getter.js
+++ data-service/data_getter.js
@@ -11,39 +11,86 @@ var contractAbi = [ { "constant": false, "inputs": [ { "name": "dataUrl", "type"
 var DataServiceContract = web3.eth.contract(contractAbi);
 
 // Get contract instance
-var contractAddress = "0x33adA680b83d86B8d20067785222bb752EFB56A8";
+var contractAddress = "0xa0fD10BE97Cca603450373CDfAEb08316e684360";
 var contract = DataServiceContract.at(contractAddress);
 
-// Get amount of data requests currently in the contract,
-// after watching for the output event.
-contract.getDataRequestLength();
-var getLengthEvents = contract.GetDataRequestLength();
-getLengthEvents.watch(function(error, getLengthResult){
-  if (error) {
-    throw error;
-  } else {
-    // We got the length.  Go through each request and fulfill it.
-    var dataRequestsLength = getLengthResult.args.length.s;
-    for(var iteration = 0; iteration < dataRequestsLength; iteration++){
-      // Get data request metadata (URL, etc)
-      contract.getDataRequest(iteration);
-      var getDataRequestEvents = contract.GetDataRequest({ id: iteration });
-      getDataRequestEvents.watch(function(error, getRequestDataResult) {
-        if (error) {
-          throw error;
-        } else {
-          // We got the metadata (including the URL), so make the request.
-          var dataUrl = getRequestDataResult.args.dataurl;
-          Request(dataUrl, function (error, response, body) {
-            // Add the response data to the contract
-            if (error) {
-              contract.addDataPoint(iteration, false);
-            } else {
-              contract.addDataPoint(iteration, true, body);
-            }
-          });
-        }
-      });
+// General flow of code is:
+// 1. Get the amount of data requests that we need to process.  This allows us to iterate through them all.
+// 2. Go through each data request and get the metadata for that request (e.g. the URL to request).
+// 3. Make the requested request (haha), and upload the result to the contract.
+// Pretty simple, right?
+
+// Retrieves length
+var retrieveDataRequestLength = function() {
+  console.log("Retrieving data request length");
+
+  // First we need to define the event handler, then we can make the actual request.
+  var getLengthEvents = contract.GetDataRequestLength();
+  getLengthEvents.watch(function(error, getLengthResult){
+    if(error) {
+      console.log(error);
+    } else {
+      var lengthActual = getLengthResult.args.length.s;
+      console.log("Retrieved data request length", lengthActual);
+
+      onDataRequestLengthRetrieved(lengthActual);
     }
+  });
+
+  // Makes the actual request
+  contract.getDataRequestLength();
+}
+
+// Iterates through each request and processes it
+var onDataRequestLengthRetrieved = function(dataRequestLength) {
+  for(var iteration = 0; iteration < dataRequestLength; iteration++){
+    onDataRequestProcess(iteration);
   }
-});
+}
+
+// Processes the request by getting the data request URL
+var onDataRequestProcess = function(dataRequestId) {
+  console.log("Processing data request", dataRequestId);
+
+  // First we need to define the event handler, then we can make the actual request.
+  var getDataRequestEvents = contract.GetDataRequest({ id: dataRequestId });
+  getDataRequestEvents.watch(function(error, getRequestDataResult) {
+    if (error) {
+      console.log(error);
+    } else {
+      // We got the metadata (including the URL)
+      var dataRequestUrl = getRequestDataResult.args.dataurl;
+      console.log("Got metadata for data request", dataRequestId, dataRequestUrl);
+
+      onDataRequestUrlRetrieved(dataRequestId, dataRequestUrl);
+    }
+  });
+
+  // Makes the actual request
+  contract.getDataRequest(dataRequestId);
+}
+
+// Gets the response body for the provided dataRequest ID.  Assumes request URL matches the request ID.
+var onDataRequestUrlRetrieved = function(dataRequestId, dataRequestUrl) {
+  console.log("Making HTTP request", dataRequestId, dataRequestUrl);
+
+  Request(dataRequestUrl, function (error, response, body) {
+    console.log("HTTP response received", dataRequestId, error, response, body);
+
+    onDataRequestResponseBodyRetrieved(dataRequestId, error, body);
+  });
+}
+
+// Uploads the response data to the contract
+var onDataRequestResponseBodyRetrieved = function(dataRequestId, error, body) {
+  console.log("Adding HTTP response to contract", dataRequestId, error, body);
+
+  if (error) {
+    contract.addDataPoint(dataRequestId, false, "");
+  } else {
+    contract.addDataPoint(dataRequestId, true, body);
+  }
+}
+
+// Kickoff the chain of events
+retrieveDataRequestLength();
